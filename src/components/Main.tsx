6e6728431e8979bb9171f6d49689ac9c70099401
Gabe Abud <gabriel.jabud@gmail.com>
2020-05-17 13:37:21 -0700

    add HTML5 input validation

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -139,15 +139,15 @@ const Main: FC<MainPropsType> = (props) => {
           <input type="hidden" name="bot-field" />
           <div className="field half first">
             <label htmlFor="name">Name</label>
-            <input type="text" name="name" id="name" />
+            <input type="text" name="name" id="name" required />
           </div>
           <div className="field half">
             <label htmlFor="email">Email</label>
-            <input type="text" name="email" id="email" />
+            <input type="email" name="email" id="email" required />
           </div>
           <div className="field">
             <label htmlFor="message">Message</label>
-            <textarea name="message" id="message" rows={4}></textarea>
+            <textarea name="message" id="message" rows={4} required></textarea>
           </div>
           <ul className="actions">
             <li>
